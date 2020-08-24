b96c8adf8f0bd56b77de80b7f0463882a250a0de
SIDDHANTA-DUTTA <64474901+SIDDHANTA-DUTTA@users.noreply.github.com>
2020-08-24 21:15:58 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,9 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    document.getElementById("image").innerHTML = previewPic.alt;
+}
+   
+function unDo(){
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+}
